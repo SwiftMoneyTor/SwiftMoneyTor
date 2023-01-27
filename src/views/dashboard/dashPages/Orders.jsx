bbc52411fe447fac5e3debed238b9141adc3fa5c
OnePinch <112742526+WhistnerRojas@users.noreply.github.com>
2023-01-27 16:59:51 +0800

    Delete src directory

--- src/views/dashboard/dashPages/Orders.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Orders(){
-    return (
-        <h2 className="fs-3">Orders Content</h2>
-    )
-}
--- src/views/dashboard/dashPages/Reports.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Reports(){
-    return (
-        <h2 className="fs-3">Reports Content</h2>
-    )
-}
