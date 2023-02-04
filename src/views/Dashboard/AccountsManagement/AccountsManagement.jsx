cdfba1a80bd3286683780a882a18cf16b4e4c660
Whistner Rojas <whistner.rojas.fg@gmail.com>
2023-02-04 08:53:08 +0800

    user accounts ui

--- src/views/Dashboard/AccountsManagement/AccountsManagement.jsx
+++ src/views/Dashboard/AccountsManagement/AccountsManagement.jsx
@@ -5,7 +5,11 @@ import './accounts.css';
 import defaultPic from '../../../assets/dashboard/avatar.png';
 import React from 'react';
 import ProfileForm from './accountsComponent/profileForm';
+import AccSettings from './accountsComponent/accSettings';
+import AccHelp from './accountsComponent/accHelp';
+import AccProfileUpload from './accountsComponent/accProfileUpload';
 import {AiOutlineUpload, AiOutlineForm, AiOutlineSave} from 'react-icons/ai';
+
 
 const AccountsManagement = () => {
 
@@ -13,58 +17,94 @@ const AccountsManagement = () => {
     const [profileInfo, setProfileInfo] = React.useState({
         firstName : 'Whistner',
         lastName : 'Rojas',
-        age: 25,
-        lotNum : '2',
+        bDay: '1992/01/10',
+        lotNum : 2,
         street: 'Bulusan',
         brgy: 'Paang Bundok',
         city: 'Quezon City',
-        zipCode: '1114'
+        zipCode: 1114,
+        email: 'whistner@gmail.com',
+        password: '123alpha',
+        profilePic: 'default',
     });
 
-    const editField = true
-    const [editInfo, setEditInfo] = React.useState( editField )
+    const [editInfo, setEditInfo] = React.useState(true)
 
     const clickToEdit = (event)=> {
         setEditInfo((oldstate)=> oldstate = !oldstate)
     };
 
-    const clickToSave = ()=> {
-        setEditInfo((oldstate)=> oldstate = !oldstate)
+    const clickToSave = (event)=>{
+        event.preventDefault()
+        clickToEdit()
     };
+
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        setProfileInfo((prevInfo)=> ({
+            ...prevInfo,
+            [name] : value
+        }))
+    }
+
+    const [links, setLinks] = React.useState('Profile')
+
+    const accLink = (event) =>{
+        const { id, innerHTML} = event.target
+        id !='Upload' ? setLinks((link)=> link = innerHTML) : setLinks((link)=> link = id)
+    }
 
     return (
         <div className="col-md-6 container-lg border border-3 rounded-3 mt-5">
             <div className="row d-flex justify-content-center">
-                <div className="col-4 col-lg-6 col-xl-2 d-flex flex-column py-5 leftPane">
+                <div className="col-4 col-lg-6 col-xl-2 d-flex flex-column align-content-center py-5 leftPane">
                     <div className="profilePic-account pb-2">
                         <img src={`${defaultPic}`}/>
                         <div className='uploadButton col-auto w-100'>
-                            <span className='fs-3'><AiOutlineUpload title='upload'/></span>
+                            <span className='fs-3' style={{cursor: 'pointer'}}><AiOutlineUpload title='Upload' className='mb-2' id='Upload' onClick={accLink}/></span>
                         </div>
                     </div>
-                    <Nav className="flex-column fs-5 border-end border-1 accLink">
-                        <Nav.Item className='py-2'>
-                            <NavLink>Profile</NavLink>
-                        </Nav.Item>
-                        <Nav.Item className='py-2'>
-                            <NavLink>Account</NavLink>
-                        </Nav.Item>
-                        <Nav.Item className='py-2'>
-                            <NavLink>Password</NavLink>
-                        </Nav.Item>
-                        <Nav.Item className='py-2'>
-                            <NavLink>Help</NavLink>
-                        </Nav.Item>
-                    </Nav>
+                    <div className=''>
+                        <Nav className="flex-column fs-5 align-content-center border-end border-2 accLink">
+                            <Nav.Item className='py-2'>
+                                <NavLink onClick={accLink}>Profile</NavLink>
+                            </Nav.Item>
+                            <Nav.Item className='py-2'>
+                                <NavLink onClick={accLink}>Account</NavLink>
+                            </Nav.Item>
+                            <Nav.Item className='py-2'>
+                                <NavLink onClick={accLink}>Help</NavLink>
+                            </Nav.Item>
+                        </Nav>
+                    </div>
                 </div>
-                <div className="col-8 col-lg-6 pt-5 d-flex justify-content-center rightPane">
+                <div className="col-8 col-lg-6 d-flex py-md-5 py-sm-2 flex-column justify-content-center rightPane">
 
-                    <ProfileForm 
-                    editInfo={editInfo} 
-                    toClick={editInfo === true ? clickToEdit : clickToSave}
-                    profileInfo={profileInfo}
-                    />
-                    
+                    {
+                        links === 'Profile' && <ProfileForm 
+                                                    editInfo={editInfo} 
+                                                    toClick={editInfo === true ? clickToEdit : clickToSave}
+                                                    profileInfo={profileInfo}
+                                                    submit={clickToSave}
+                                                    onChange={handleChange}
+                                                />
+                    }
+                    {
+                        links === 'Account' && <AccSettings 
+                                                    editInfo={editInfo} 
+                                                    toClick={editInfo === true ? clickToEdit : clickToSave}
+                                                    profileInfo={profileInfo}
+                                                    submit={clickToSave}
+                                                    onChange={handleChange}
+                                                />
+                    }
+                    {
+                        links === 'Help' && <AccHelp />
+                    }
+                    {
+                        links === 'Upload' && <AccProfileUpload />
+                    }
+
                 </div>
             </div>
         </div>
--- src/views/Dashboard/AccountsManagement/accountsComponent/profileForm.jsx
+++ src/views/Dashboard/AccountsManagement/accountsComponent/profileForm.jsx
@@ -1,37 +1,117 @@
-import {AiOutlineUpload, AiOutlineForm, AiOutlineSave} from 'react-icons/ai';
+import { AiOutlineForm, AiOutlineSave} from 'react-icons/ai';
 
 export default function ProfileForm(props){
 
-    const {firstName, lastName, age, lotNum, street, brgy, city, zipCode} = props.profileInfo
+    const {firstName, lastName, bDay, lotNum, street, brgy, city, zipCode} = props.profileInfo
 
     return(
-        <form className='col-md-12 col-lg-10 col-xl-12'>
-                        <div className="d-flex justify-content-end pt-5 pb-2">
-                            <a className="input-group-text fs-4" 
-                                onClick={props.toClick}>
-                                {props.editInfo === false ? <AiOutlineSave title='Save' className='text-success'/> : <AiOutlineForm title='Edit' className='text-danger'/>}
-                            </a>
-                        </div>
-                        <div className="input-group mb-3">
-                            <span className="input-group-text">First name</span>
-                            <input 
-                            type="text" 
-                            className="form-control"
-                            name='firstname'
-                            value={firstName}
-                            disabled={props.editInfo}
-                            />
-                        </div>
-                        <div className="input-group mb-3">
-                            <span className="input-group-text">Last name</span>
-                            <input 
-                            type="text" 
-                            className="form-control"
-                            name='lastName'
-                            value={lastName}
-                            disabled={props.editInfo}
-                            />
-                        </div>
-                    </form>
+        <form onSubmit={props.submit} method="POST" className='col-md-12 col-lg-10 col-xl-12'>
+            <div className="d-flex justify-content-end py-2 pb-2">
+                <span className="input-group-text fs-4 editToSave" 
+                    onClick={props.toClick} data-edit="disableEditAccountField">
+                    {props.editInfo=== false ? <AiOutlineSave title='Save' className='saveEdit'/> : <AiOutlineForm title='Edit' className='editSave'/>}
+                </span>
+            </div>
+            <div className="input-group mb-3">
+                <span className="input-group-text">First name</span>
+                <input 
+                type="text" 
+                className="form-control"
+                name='firstName'
+                value={firstName}
+                disabled={props.editInfo}
+                onChange={props.onChange}
+                />
+            </div>
+            <div className="input-group mb-3">
+                <span className="input-group-text">Last name</span>
+                <input 
+                type="text" 
+                className="form-control"
+                name='lastName'
+                value={lastName}
+                disabled={props.editInfo}
+                onChange={props.onChange}
+                />
+            </div>
+            <div className="input-group mb-3">
+                <span className="input-group-text">Birthday</span>
+                <input 
+                type="text" 
+                className="form-control"
+                name='bDay'
+                value={bDay}
+                disabled={props.editInfo}
+                onChange={props.onChange}
+                />
+            </div>
+            <div className="row">
+                <div className="col-lg-6">
+                    <div className="input-group mb-3">
+                        <span className="input-group-text">Lot#</span>
+                        <input 
+                        type="text" 
+                        className="form-control"
+                        name='lotNum'
+                        value={lotNum}
+                        disabled={props.editInfo}
+                        onChange={props.onChange}
+                        />
+                    </div>
+                </div>
+                <div className="col-lg-6">
+                    <div className="input-group mb-3">
+                        <span className="input-group-text">Street</span>
+                        <input 
+                        type="text" 
+                        className="form-control"
+                        name='street'
+                        value={street}
+                        disabled={props.editInfo}
+                        onChange={props.onChange}
+                        />
+                    </div>
+                </div>
+                <div className="input-group mb-3">
+                    <span className="input-group-text">Brgy</span>
+                    <input 
+                    type="text" 
+                    className="form-control"
+                    name='brgy'
+                    value={brgy}
+                    disabled={props.editInfo}
+                    onChange={props.onChange}
+                    />
+                </div>
+                <div className="col-xl-6">
+                    <div className="input-group mb-3">
+                        <span className="input-group-text">City</span>
+                        <input 
+                        type="text" 
+                        className="form-control"
+                        name='city'
+                        value={city}
+                        disabled={props.editInfo}
+                        onChange={props.onChange}
+                        />
+                    </div>
+                </div>
+                <div className="col-xl-6">
+                    <div className="input-group mb-3">
+                        <span className="input-group-text">Zip Code</span>
+                        <input 
+                        type="text" 
+                        className="form-control"
+                        name='zipCode'
+                        value={zipCode}
+                        disabled={props.editInfo}
+                        onChange={props.onChange}
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <button className='displayNone'></button>
+        </form>
     )
 }
