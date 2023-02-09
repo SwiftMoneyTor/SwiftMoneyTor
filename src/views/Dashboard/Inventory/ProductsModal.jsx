10b12ba28cf516416fb1f5adcfa0e00c7cd2ab5c
ellenjane001 <ellenjane.000001998@gmail.com>
2023-02-09 16:17:16 +0800

    added products management modal

--- src/views/Dashboard/Inventory/ProductsModal.jsx
+++ src/views/Dashboard/Inventory/ProductsModal.jsx
@@ -2,16 +2,23 @@ import { Button, FormControl, MenuItem, Stack, TextField } from "@mui/material";
 import { useState } from 'react';
 import { Modal } from "react-bootstrap";
 import { useForm } from "react-hook-form";
+import useAppStore from '../../../appStore';
+import FetchWithFormData from "../../../utils/API/FetchWithFormData";
 
 
 const ProductsModal = () => {
     const { register, handleSubmit, formState: { errors }, clearErrors } = useForm()
+    const credentials = useAppStore(state => state.credentials)
     const [show, setShow] = useState(false);
     const [select, setSelect] = useState(1)
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const handleSubmition = (data) => {
-        console.log(data)
+        const formData = new FormData()
+        formData.append('product_image', data.product_image[0])
+        formData.append('product_name', data.product_name)
+        formData.append('category_id', data.category)
+        FetchWithFormData(credentials.token, formData)
     }
     const categories = [
         { name: 'Category 1', id: 1 },
@@ -23,7 +30,7 @@ const ProductsModal = () => {
         setSelect(e.target.value)
     }
     return (<>
-        
+
         <Button variant="contained" onClick={handleShow} color="success">
             Add Product
         </Button>
@@ -48,7 +55,10 @@ const ProductsModal = () => {
                             <TextField label="Product Name" {...register('product_name')} />
                         </FormControl>
                         <FormControl variant="outlined">
-                            <TextField label="Image" {...register('product_image')} />
+                            <Button variant="contained" color="success" component="label">
+                                Upload
+                                <input accept="image/*" hidden type="file" {...register('product_image')} />
+                            </Button>
                         </FormControl>
                         <Stack spacing={1} direction="row" justifyContent="flex-end">
                             <Button variant="contained" onClick={handleClose} color="warning" type="button">
@@ -59,7 +69,6 @@ const ProductsModal = () => {
                             </Button>
                         </Stack>
                     </Stack>
-
                 </form>
             </Modal.Body>
         </Modal>
