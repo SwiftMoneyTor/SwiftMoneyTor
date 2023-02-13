766e18d6f69b6129b9c89b11a038874f8e1e6e1d
ellenjane001 <ellenjane.000001998@gmail.com>
2023-02-13 17:52:16 +0800

    added inventory function

--- src/views/Dashboard/DashContainer/DashContainer.jsx
+++ src/views/Dashboard/DashContainer/DashContainer.jsx
@@ -1,22 +1,7 @@
-import { useEffect } from "react"
-import useAppStore from "../../../appStore"
-import avatar from '../../../assets/dashboard/avatar.png'
-import FetchWithAuth from "../../../utils/API/Fetch/FetchWithAuth"
 import DashBoardNav from "../DashBoardNav/DashBoardNav"
 import Main from "../Main/Main"
 
 const DashContainer = () => {
-    const setProfilePic = useAppStore(state => state.setProfilePic)
-    const credentials = useAppStore(state => state.credentials)
-    useEffect(() => {
-        let data = { user_id: credentials.id }
-        FetchWithAuth('profile/fetch/image', credentials.token, data).then(response => {
-            if (response.responsedata.display_image !== '')
-                setProfilePic(response.responsedata.display_image)
-            else
-                setProfilePic(avatar)
-        })
-    }, [])
     return (
         <div>
             <DashBoardNav />
